fa91c5fd09091b6ff8d3563caa593448011186c5
liambloom <liamrbloom@gmail.com>
2019-03-24 18:09:16 -0400

    Update serverV3.js

--- servers/serverV3.js
+++ servers/serverV3.js
@@ -1,5 +1,4 @@
 //jshint esversion:8
-//const http = require("http");//Do I need this?
 const express = require("express");
 const url = require("url");
 const fs = require("fs");
@@ -10,146 +9,75 @@ const port = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
 
-const path = req => url.parse(req.url, true).pathname;
-const filetype = req => req.match(/(?<=\.)[^.\/]+$/);//Does this work
+const path = req => url.parse(`${req.protocol}://${req.get("host")}${req.originalUrl}`, true);
+const filetype = req => req.match(/(?<=\.)[^.\/]+$/);
+
 const serve = (req, res, page, loop = 0, status = 200) => {
 	fs.readFile(page, (err, data) => {
+		//If non-ejs file not found
 		if (err && filetype(page) !== null) {
+			//if this is the first try, server 404
 			if (loop <= 0) {
 				serve(req, res, "./404", loop + 1, 404);
 			}
+			//else, serve 500
 			else {
-				//console.log(filetype(page));
 				res.writeHead(500, { "Content-Type": "text/html" });
 				res.write(`The page ${path(req).href} could not be found<br>${err}`);
 				res.end();
 			}
 		}
-		else {
-			if (filetype(page) !== null) {
-				try {
-					res.writeHead(status, { "Content-Type": mime.contentType(filetype(page)[0]) });
-					res.write(data);
-					res.end();
-				}
-				catch (error) {
-					res.writeHead(500, { "Content-Type": "text/html" });
-					res.write(`The page ${path(req).href} could not be found<br>${error}`);
-					res.end();
-				}
-			}
-			else {
-				//try {
-					res.render(page,/* undefined,*/ (error, html) => {
-						if (error && loop <= 0) {
-							serve(req, res, "./404", loop + 1, 404);
-						}
-						else if (html) {
-							res.writeHead(status, { "Content-Type": "text/html" });
-							res.write(html);
-							res.end();
-						}
-						else {
-							res.writeHead(500, { "Content-Type": "text/html" });
-							res.write(`The page ${path(req).href} could not be found`);
-							res.end();
-						}
-					});
-					//Do I need this?
-				/*}
-				catch (error) {
-					serve(req, res, "./404/index", loop + 1, 404);
-				}*/
-			}
-			/*try {	
-				//Does || do what I think it does?
-				if (filetype(page) !== null) {
-					res.writeHead(status, { "Content-Type": mime.contentType(filetype(page)[0]) });
-					res.write(data);
-				}
-				else {
-					res.writeHead(status, { "Content-Type": "test/html" });
-					res.render(page);
-				}
+		//else if non-ejs file found
+		else if (filetype(page) !== null) {
+			//try serving page
+			try {
+				res.writeHead(status, { "Content-Type": mime.contentType(filetype(page)[0]) });
+				res.write(data);
 				res.end();
 			}
+			//if error, serve 500
 			catch (error) {
-				if (filetype(page) !== null) {
-					res.writeHead(500, { "Content-Type": "text/html" });
-					res.write(`The page ${path(req).href} could not be found<br>${error}`);
+				res.writeHead(500, { "Content-Type": "text/html" });
+				res.write(`The page ${path(req).href} could not be found<br>${error}`);
+				res.end();
+			}
+		}
+		else {
+			res.render(page, { target: path(req).href }, (error, html) => {
+				//if ejs file not found and this is first try, serve 404
+				if (error && loop <= 0) {
+					serve(req, res, "./404", loop + 1, 404);
+				}
+				//else if ejs file found, serve it
+				else if (html) {
+					res.writeHead(status, { "Content-Type": "text/html" });
+					res.write(html);
 					res.end();
 				}
+				//if the 404 didn't work, serve 500
 				else {
-					serve(req, res, "./404/index", loop + 1, 404);
+					res.writeHead(500, { "Content-Type": "text/html" });
+					res.write(`The page ${path(req).href} could not be found`);
+					res.end();
 				}
-			}*/
+			});
 		}
 	});
 };
-/*const err404 = (req, res, err) => {
-	serve(req, res, "./404/index.html", (req, res) => {
-		err500(req, res);
-	}, 404);
-};
-const err500 = (req, res, error) => {
-	
-};*/
 
+//ejs index server
 app.get(/\/$/, (req, res) => {
-	//serve(req, res, `.${path(req)}index.html`);
-	//console.log("index.ejs server ran");
-	/*res.writeHead(status || 200, { "Content-Type": mime.contentType(filetype(page)) });
-	res.render(`.${path(req)}index`);
-	res.end();*/
-	serve(req, res, `.${path(req)}index`);
+	serve(req, res, `.${path(req).pathname}index`);
 });
 
+//regular ejs server
 app.get(/\/[^\/.]+$/, (req, res) => {
-	//serve(req, res, `.${path(req)}`);
-	//console.log(res.render("./views/header"))
-	//console.log(".ejs server ran");
-	/*try {
-		res.writeHead(status || 200, { "Content-Type": mime.contentType(filetype(page)) });
-		res.render(`.${path(req)}`);
-		res.end();
-	}
-	catch (err) {
-
-	}*/
-	serve(req, res, `.${path(req)}`);
-	//res.render("test");
+	serve(req, res, `.${path(req).pathname}`);
 });
 
+//non-ejs server
 app.get(/\/[^]+\.[^]+$/, (req, res) => {
-	//console.log("Regular server ran");
-	serve(req, res, `.${path(req)}`);
+	serve(req, res, `.${path(req).pathname}`);
 });
 
-/*app.route("/css/theme-sugestions.json")
-	.get((req, res) => {
-		serve(req, res, "../css/theme-sugestions.json");
-	})
-	.post((req, res) => {
-		fs.readFile("../css/theme-sugestions.json", (err, data) => {
-			if (err) throw err;
-			else {
-				let sugestions = JSON.parse(data);
-				sugestions.sugestions.push(JSON.parse(url.parse(req.url, true).query));
-				fs.writeFile("../css/theme-sugestions.json", JSON.stringify(sugestions), (err) => {
-					if (err) {
-						res.status(500).send(sugestions);
-						throw err;
-					}
-					else {
-						res.status(201).send(sugestions);
-					}
-				});
-				//console.log(url.parse(req.url, true).query);
-			}
-		});
-	})//Do I need a ;
-	
-
-app.post("/css/");*/
-
-app.listen(port, () => { console.log(`Server running on port ${port}...`); });
+app.listen(port);
