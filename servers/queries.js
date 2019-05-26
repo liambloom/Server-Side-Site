f4e5b38df0fe90333d45561ce87ae215f6a689ee
liambloom <liamrbloom@gmail.com>
2019-05-25 20:50:21 -0400

    removed node modules folder

--- servers/queries.js
+++ servers/queries.js
@@ -1,7 +1,7 @@
 //jshint esversion:9
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt"); // This works differently depending on the os it is compiled on, so must have a seperate download for local and server
 const os = require("os");
 const uuid = require("uuid/v4");
 const { Pool } = require("pg");
