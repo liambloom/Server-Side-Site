29328969c9316035a56edd21559b3063a7cb4059
liambloom <liamrbloom@gmail.com>
2019-06-25 21:19:37 -0400

    Redesigned error catching system for query page
    insetad of
    if (err) handle(err)
    things are
    .catch(handle)

--- servers/initPool.js
+++ servers/initPool.js
@@ -3,12 +3,18 @@ const os = require("os");
 const bcrypt = require("bcrypt");
 const uuid = require("uuid/v4");
 const url = require("url");
-//const shortHash = require("short-hash");
 const fs = require("fs");
 const mail = require("./mail");
 const { Pool } = require("pg");
+
 const path = req => url.parse(`${req.protocol}://${req.get("host")}${req.originalUrl}`, true);
 const testing = os.hostname().includes("DESKTOP");
+const handle = (err) => {
+  console.error(err);
+  res.writeHead(200, { "Content-Type": "application/json; charset=utf-8" });
+  res.send(JSON.stringify({ error: err }));
+  res.end();
+};
 const testConfig = {
   user: "me",
   host: "localhost",
@@ -25,12 +31,13 @@ const pool = new Pool((testing) ? testConfig : herokuConfig);
 pool.on("error", (err) => {
   //Handle error
 });
+
 module.exports = {
   pool,
   bcrypt,
   uuid,
   path,
   mail,
-  //shortHash,
+  handle,
   fs
 };
--- servers/queries.js
+++ servers/queries.js
@@ -1,8 +1,8 @@
 //jshint esversion:9
 const { mail, initPool } = require("./mail");
-const { bcrypt, uuid, pool, path, /*mail, */fs } = initPool;//require("./initPool");
+const { bcrypt, uuid, pool, path, handle, fs } = initPool;//require("./initPool");
 
-const createTable = (req, res) => {
+const createTable = () => {
   //pool.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
   pool.query(`
     CREATE TABLE IF NOT EXISTS users (
@@ -39,115 +39,110 @@ const createTable = (req, res) => {
   `);
 };
 const login = (req, res, userid) => {
-  return new Promise((resolve, reject) => {
-    //console.log("logged in");
-    const id = uuid();
-    pool.query("INSERT INTO sessions (sessionid, userid) VALUES ($1, $2)", [id, userid/*, new Date(new Date().getTime() + 1000 * 60 * 60 * 24).toISOString().replace("T", " ").replace(/[a-z]*$/i, "")*/], (err, data) => {
-      //console.log(err);
-      if (err) reject(err);
-      else {
-        req.session.user = id;
-        resolve();
-      }
+  return Promise.resolve()
+    .then(() => {
+      const id = uuid();
+      pool.query("INSERT INTO sessions (sessionid, userid) VALUES ($1, $2)", [id, userid])
+        .then(data => {
+          req.session.user = id;
+        });
     });
-  });
 };
 const newUser = (req, res, id, username, password, email, color, light) => {
-  pool.query("INSERT INTO users (id, username, password, email, color, light, type, since) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)", [id, username, password, email, color, light, "USER", "today"/*d.toISOString().split("T")[0]*/], (err, data) => {
-    if (err) res.status(500).send(err).end();
-    else {
-      //req.session.user = id;
+  pool.query("INSERT INTO users (id, username, password, email, color, light, type, since) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)", [id, username, password, email, color, light, "USER", "today"])
+    .then(data => {
       login(req, res, id)
         .then(() => { res.status(201).end(); })
-        .catch(err => { res.status(500).end(err); });
-      //res.status(201).end();
-    }
-  });
+        .catch(handle);
+    })
+    .catch(handle);
 };
 const getAll = (req, res) => {
-  pool.query("SELECT * FROM users", (err, data) => {
-    if (err) res.status(500).send(err).end();
-    else res.status(200).json(data.rows);
-  });
+  pool.query("SELECT * FROM users")
+    .then(data => {
+      if (err) res.status(500).send(err).end();
+      else res.status(200).json(data.rows);
+    })
+    .catch(handle);
 };
 const get = (id, callback) => {
   //const id = parseInt(req.params.id);
 
-  pool.query("SELECT * FROM users WHERE id = $1", [id]/*, (err, data) => { // $1 is a variable passed in as [id]
-    if (err) res.status(500).send(err);
-    else res.status(200).json(data.rows);
-  }*/)
-  .then(res => res.rows[0])
-  .then(res => {
-    delete res.password;
-    return res;
-  })
-  .then(callback)
-  .catch(err => callback(undefined));
+  pool.query("SELECT * FROM users WHERE id = $1", [id])
+    .then(res => res.rows[0])
+    .then(res => {
+      delete res.password;
+      return res;
+    })
+    .then(callback)
+    .catch(err => callback(undefined));
 };
 const confirm = (req, res) => {
   const {username, password} = req.body;
 
-  pool.query("SELECT id, password FROM users WHERE username=$1", [username], (error, data) => {
-    if (error) {
-      return res.status(500).send(error).end();
-    }
-    if (data.rows.length > 0) {
-      const user = data.rows[0];
-      bcrypt.compare(password, user.password, (err, result) => {
-        if (err) res.status(500).send(err).end();
-        if (result) {
-          //req.session.user = user.id;
-          login(req, res, user.id)
-            .then(() => { res.status(204).end(); })
-            .catch(err => { res.status(500).end(err); });
-          
-        }
-        else res.status(403).end();
-      });
-    }
-    else res.status(401).end();
-  });
+  pool.query("SELECT id, password FROM users WHERE username=$1", [username])
+    .then(data => {
+      if (data.rows.length > 0) {
+        const user = data.rows[0];
+        bcrypt.compare(password, user.password)
+          .then(result => {
+            if (result) {
+              login(req, res, user.id)
+                .then(() => { res.status(204).end(); })
+                .catch(handle);
+            }
+            else res.status(403).end();
+          })
+          .catch(handle);
+      }
+      else res.status(401).end();
+    })
+    .catch(handle);
 };
 const create = (req, res) => {
   const { username, password, email, color, light } = req.body;
-  bcrypt.hash(password, 10, (e, hash) => {
-    if (e) res.status(500).send(e).end();
-    const id = uuid();
-    pool.query("SELECT id FROM users WHERE username = $1", [username], (error, user) => {
-      if (error) res.status(500).send(error).end();
-      else if (user.rows[0]) res.status(409).end();
-      else {
-        if (!email) newUser(req, res, id, username, hash, email, color, light);
-        else {
-          const code = uuid();
-          pool.query("INSERT INTO confirm (userid, code, email) VALUES ($1, $2, $3)", [id, code, email]); // Is this valid syntax, or is the callback required?
-          fs.readFile("./json/themes.json", (err, data) => {
-            if (err) res.status(500).end(err);
+  bcrypt.hash(password, 10)
+    .then(hash => {
+      const id = uuid();
+      pool.query("SELECT id FROM users WHERE username = $1", [username])
+        .then(user => {
+          if (user.rows[0]) res.status(409).end();
+          else {
+            if (!email) newUser(req, res, id, username, hash, email, color, light);
             else {
-              newUser(req, res, id, username, hash, null, color, light);
-              const theme = JSON.parse(data)[color];
-              const newTheme = {
-                light: theme.gradientLight,
-                dark: theme.gradientDark,
-                headTxt: theme.headTextColor,
-                color: color
-              };
-              if (light === "dark") {
-                newTheme.bg = theme.offBlack;
-                newTheme.txt = theme.headTextColor;
-              }
-              else {
-                newTheme.bg = theme.offWhite;
-                newTheme.txt = theme.offBlack;
-              }
-              mail.confirm(res, email, username, newTheme, code, path(req));
+              const code = uuid();
+              pool.query("INSERT INTO confirm (userid, code, email) VALUES ($1, $2, $3)", [id, code, email])
+                .then(() => {
+                  fs.readFile("./json/themes.json", (err, data) => {
+                    if (err) handle(err);
+                    else {
+                      newUser(req, res, id, username, hash, null, color, light);
+                      const theme = JSON.parse(data)[color];
+                      const newTheme = {
+                        light: theme.gradientLight,
+                        dark: theme.gradientDark,
+                        headTxt: theme.headTextColor,
+                        color: color
+                      };
+                      if (light === "dark") {
+                        newTheme.bg = theme.offBlack;
+                        newTheme.txt = theme.headTextColor;
+                      }
+                      else {
+                        newTheme.bg = theme.offWhite;
+                        newTheme.txt = theme.offBlack;
+                      }
+                      mail.confirm(res, email, username, newTheme, code, path(req));
+                    }
+                  });
+                })
+                .catch(handle); // Is this valid syntax, or is the callback required?
             }
-          });
-        }
-      }
-    });
-  });
+          }
+        })
+        .catch(handle);
+    })
+    .catch(handle);
 };
 const update = (req, res) => {
   const id = req.user.id;
@@ -155,61 +150,52 @@ const update = (req, res) => {
   if (/id|username|password|since/.test(category)) res.status(400).send("These categories cannot be updated").end();
   if (category === "type" && req.user.type !== "ADMIN") res.status(403).send("This is an admin only action").end();
 
-  pool.query(`UPDATE users SET ${category} = $1 WHERE id = $2`, [value, id], (err, data) => {
-    if (err) {
-      res.status(500).send(err).end();
-      console.error(err);
-    }
-    else res.status(204).end();
-  });
+  pool.query(`UPDATE users SET ${category} = $1 WHERE id = $2`, [value, id])
+    .then(data => {
+      res.status(204).end();
+    })
+    .catch(handle);
 }; 
 update.fromEmailConfirm = (req, res) => {
   const id = req.params.addId;
-  pool.query("UPDATE users SET email = (SELECT email FROM confirm WHERE code = $1) WHERE id = (SELECT userid FROM confirm WHERE code = $1)", [id], (err, data) => {
-    //console.log(data);
-    if (err) {
-      console.log("500 error updating user");
-      res.status(500).end();
-      console.error(err);
-    }
-    else if (data.rowCount >= 1) {
-      pool.query("DELETE FROM confirm WHERE code = $1 RETURNING userid", [id], (error, data) => {
-        if (error || data.rows[0] ? !data.rows[0].userid : true) {
-          res.status(500).end();
-          console.error(err);
-        }
-        else {
-          login(req, res, data.rows[0].userid)
-            .then(() => { res.redirect(303, "/"); })
-            .catch(err => { 
-              console.error(err); 
-              res.status(500).send(err).end(); 
-            });
-        }
-      });
-    }
-    else { res.status(404).end(); console.log("no such user"); }
-  });
+  pool.query("UPDATE users SET email = (SELECT email FROM confirm WHERE code = $1) WHERE id = (SELECT userid FROM confirm WHERE code = $1)", [id])
+    .then(data => {
+      if (data.rowCount >= 1) {
+        pool.query("DELETE FROM confirm WHERE code = $1 RETURNING userid", [id])
+          .then(data => {
+            if (data.rows[0] ? !data.rows[0].userid : true) {
+              throw "No user id found";
+            }
+            else {
+              login(req, res, data.rows[0].userid)
+                .then(() => { res.redirect(303, "/"); })
+                .catch(handle);
+            }
+          })
+          .catch(handle);
+      }
+      else { res.status(404).end(); console.log("no such user"); }
+    })
+    .catch(handle);
 };
 hasEmail = (req, res) => {
   //console.log("checked email");
   const id = req.user.id;
-  pool.query("SELECT email FROM users WHERE id = $1", [id], (err, data) => {
-    if (err) {
-      res.status(500).end();
-      console.error(err);
-    }
-    else if (data.rows[0] ? data.rows[0].email : false) res.status(204).end();
-    else res.status(404).end();
-  });
+  pool.query("SELECT email FROM users WHERE id = $1", [id])
+    .then(data => {
+      if (data.rows[0] ? data.rows[0].email : false) res.status(204).end();
+      else res.status(404).end();
+    })
+    .catch(handle);
 };
 const remove = (req, res) => {
   const id = parseInt(req.params.id);
 
-  pool.query("DELETE FROM users WHERE id = $1", [id], (err, data) => {
-    if (err) res.status(500).send(err).end();
-    else res.status(204).end();
-  });
+  pool.query("DELETE FROM users WHERE id = $1", [id])
+    .then(data => {
+      res.status(204).end();
+    })
+    .catch(handle);
 };
 const logout = (req, res) => {
   req.session.reset();
@@ -219,13 +205,11 @@ const add = (req, res) => {
   const { content, type } = req.body;
   //const d = new Date();
 
-  pool.query("INSERT INTO sugestions (content, type, by, created) VALUES ($1, $2, $3, $4)", [content, type, (req.user) ? req.user.username : null, "now"/*d.toISOString().replace(/[a-z]$/i, "")*/], (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send(err).end();
-    }
-    else res.status(201).end();
-  });
+  pool.query("INSERT INTO sugestions (content, type, by, created) VALUES ($1, $2, $3, $4)", [content, type, (req.user) ? req.user.username : null, "now"])
+    .then(data => {
+      res.status(201).end();
+    })
+    .catch(handle);
 };
 const getSugestions = (req, res) => {
 
