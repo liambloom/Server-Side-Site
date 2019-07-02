0f5f06f899ebe69e64331627e70968f878640f46
liambloom <liamrbloom@gmail.com>
2019-07-01 22:18:39 -0400

    Done with fronted of password recovery

--- js/login.js
+++ js/login.js
@@ -30,61 +30,104 @@ let loadFunc = () => {
       document.getElementById("button").parentNode.setAttribute("data-err", "Invalid username or password");
     }
   };
-  document.getElementById("passwordRecovery").addEventListener("click", () => {
-    document.getElementById("content").classList.add("box");
-    document.getElementById("content").innerHTML = 'Enter your username: <br><input id="usernamePassRecover" type="text">';
-    document.getElementById("usernamePassRecover").onenter = event => {
-      let username = event.target.value;
-      if (/^[\w\-.]{1,50}$/.test(username)) {
-        event.target.error.clear();
-        modal.open("#loadingModal");
-        window.activateLoading();
-        
-        fetch("/api/recover/" + username)
-          .then(res => {
-            if (res.ok) document.getElementById("content").innerHTML = 'You were emailed a recovery code: <br><input id="recoveryCode" type="text">';
-            else throw res.status; 
+  if (document.getElementById("passwordRecovery")) {
+    document.getElementById("passwordRecovery").addEventListener("click", () => {
+      document.getElementById("content").classList.add("box");
+      document.getElementById("content").innerHTML = 'Enter your username: <br><input id="usernamePassRecover" type="text">';
+      document.getElementById("usernamePassRecover").onenter = event => {
+        let username = event.target.value;
+        if (/^[\w\-.]{1,50}$/.test(username)) {
+          event.target.error.clear();
+          modal.open("#loadingModal");
+          window.activateLoading();
+          
+          fetch("/api/recover", {
+            method: "POST",
+            body: {
+              username
+            }
           })
-          .then(() => {
-            document.getElementById("recoveryCode").onenter = event => {
-              let code = event.target.value;
-              if (/^[a-z0-9]{7}$/.test(code)) {
-                modal.open("#loadingModal");
-                activateLoading();
-                fetch("/api/recover", {
-                  body: JSON.stringify({
-                    username,
-                    code
+            .then(res => {
+              if (res.ok) document.getElementById("content").innerHTML = 'You were emailed a recovery code: <br><input id="recoveryCode" type="text">';
+              else throw res.status; 
+            })
+            .then(() => {
+              document.getElementById("recoveryCode").onenter = event => {
+                let code = event.target.value;
+                if (/^[a-z0-9]{7}$/.test(code)) {
+                  modal.open("#loadingModal");
+                  activateLoading();
+                  fetch("/api/recover", {
+                    body: JSON.stringify({
+                      username,
+                      code
+                    })
                   })
-                })
-                  .then(res => {
-                    if (res.ok) document.getElementById("content").innerHTML = 'New Password: <input id="newPassword" type="text"><br>Retype Password: <input id="passwordCheck" type="text"';
-                    else throw res;
-                  })
-                  .then(() => {
-                    
-                  })
-                  .catch(err => {
-
-                  })
-                  .finally(() => {
-
-                  });
-              }
-            };
-          })
-          .catch(status => {
-            if (status === 404) document.getElementById("usernamePassRecover").error = "No such user";
-            else document.getElementById("usernamePassRecover").error = "Something Broke :(";
-          })
-          .finally(() => {
-            modal.close();
-            deactivateLoading();
-          });
-      }
-      else event.target.error = "Invalid Username";
-    };
-  });
+                    .then(res => {
+                      if (res.ok) document.getElementById("content").innerHTML = 'New Password: <span><input id="newPassword" type="text"></span><br>Retype Password: <span><input id="passwordCheck" type="text"></span>';
+                      else throw res;
+                    })
+                    .then(() => {
+                      document.getElementById("newPassword").addEventListener("input", event => {
+                        let e = event.target;
+                        let pass = e.value;
+                        let confirm = document.getElementById("confirmPassword");
+                        if (!pass) e.error = "Password is required";
+                        else if (/[^\w!@#$%^&*()\-+`~\\|\[\]{};:'",.\/?=]/i.test(pass)) e.error = "Illegal character detected";
+                        else if (/pass?word/i.test(pass)) e.error = "Don't use the word \"password\"";
+                        else if (/(.)\1{2,}/.test(pass)) e.error = "Don't use the same character repeatedly";
+                        else if (checkConsecutive(pass.match(/\d{3,}/g))) e.error = "Don't use consecutive numbers";
+                        else if (checkConsecutive(pass.match(/[a-z]{3,}/gi))) e.error = "Don't use consecutive letters";
+                        else if (!/[a-z]/i.test(pass)) e.error = "Password must contain a letter";
+                        else if (!/\d/.test(pass)) e.error = "Password must contain a number";
+                        else if (/^[a-z\d]*$/i.test(pass)) e.error = "Password must contain a special character";
+                        else if (/^.{0,5}$/.test(pass)) e.error = "Password must be at least 6 characters long";
+                        else if (/^.{100,}$/.test(pass)) e.error = "Password may not be longer that 100 characters";
+                        else if (pass !== confirm.value && /\S/.test(confirm.value)) confirm.error = "Must match password";
+                        else { e.error.clear(); confirm.error.clear(); }
+                      });
+                      document.getElementById("passwordCheck").addEventListener("input", event => {
+                        let e = event.target;
+                        if (!e.value) e.error = "Confirm password is required";
+                        else if (e.value !== document.getElementById("password").value) e.error = "Must match password";
+                        else e.error.clear();
+                      });
+                      document.getElementById("content").addEventListener("enter", () => {
+                        if (!document.querySelector("#content :invalid")) {
+                          fetch("/api/recover", {
+                            method: "PUT",
+                            body: {
+                              username,
+                              code,
+                              password: document.getElementById("newPassword").value
+                            }
+                          })
+                            .then(res => {
+                              if (res.ok) location.assign(new URLSearchParams(location.search).get("u"));
+                              else throw res;
+                            });
+                        }
+                      });
+                    })
+                    .catch(() => {
+                      document.getElementById("newPassword").parentNode.error = "Something Broke :(";
+                    });
+                }
+              };
+            })
+            .catch(status => {
+              if (status === 404) document.getElementById("usernamePassRecover").error = "No such user";
+              else document.getElementById("usernamePassRecover").error = "Something Broke :(";
+            })
+            .finally(() => {
+              modal.close();
+              deactivateLoading();
+            });
+        }
+        else event.target.error = "Invalid Username";
+      };
+    });
+  }
 };
 
 if (document.readyState === "complete") loadFunc();
