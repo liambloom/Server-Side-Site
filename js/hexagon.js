c079fd52d9083efc0cee5d52d71f13f9062aa384
liambloom <liamrbloom@gmail.com>
2019-07-31 00:30:22 -0400

    Fixed stopping bugs with hexagon
    The changes I made to allow for constant speed broke the slowing and stopping code, which I changed

--- js/hexagon.js
+++ js/hexagon.js
@@ -1,69 +1,79 @@
 window.hex = () => {
-  let c = document.getElementById("canvas"); // Get canvas element
+  fpsMonitor();
+  let c = document.getElementById("hex"); // Get canvas element
   c.height = 300; // Set height
   c.width = 300; // Set width
   let ctx = c.getContext('2d'); // Get canvas object
-  let size = 100; // radius at points
-  let x = 100; // center x
-  let y = 100; // center y
-  let count;
-  fpsMonitor();
+  hex.size = 100; // radius at points
+  let x = 150; // center x
+  let y = 150; // center y
+  let fpsArr;
+  let framerate = fps || 60;
 
   hex.draw = i => {
-    ctx.clearRect(0, 0, c.width, c.height); // remove previous frame
+    hex.clear(); // remove previous frame
     ctx.beginPath(); // begin a new shape
-    ctx.moveTo(x + size * Math.cos(0), y + size * Math.sin(0));// start at (x+size, 0), or rightmost point, because cos(0)=1 and sin(0)=0
+    ctx.moveTo(x + hex.size * Math.cos(0), y + hex.size * Math.sin(0));// start at (x+hex.size, 0), or rightmost point, because cos(0)=1 and sin(0)=0
 
     for (let side = 0; side <= 6; side++) { // For 6 sides
-      ctx.lineTo(x + (size - i) * Math.cos(side * 2 * Math.PI / 6), y + size * Math.sin(side * 2 * Math.PI / 6)); // if i is one, the hexagon is normal. As i aproaches size, the width shrinks linearly
-      //when i===0, the hexagon will be full size. When i===size, the hexagon will disapear
+      ctx.lineTo(x + (hex.size - i) * Math.cos(side * 2 * Math.PI / 6), y + hex.size * Math.sin(side * 2 * Math.PI / 6)); // if i is one, the hexagon is normal. As i aproaches hex.size, the width shrinks linearly
+      //when i===0, the hexagon will be full hex.size. When i===hex.size, the hexagon will disapear
     }
     
     ctx.fillStyle = themes[theme.color].gradientLight;//"#333333"; // Grey
     ctx.fill(); // Fill up the hexagon
-    return 2 * (size - i); // return hexagon width
+    return 2 * (hex.size - i); // return hexagon width
   };
 
   hex.spin = (rpm, until) => { // How did I figure this out? I typed random numbers into desmos until they did what I wanted.
+    hex.stop(); // Stop current spinning
+
     if (typeof rpm !== "number" || Math.abs(rpm) === Infinity) throw rpm + " is not a valid rpm";
     if (typeof until !== "number") until = Infinity;
     let i = 0;
-    let iMax = (2 * Math.PI * until) / 9; // Translate from degrees to input for draw funciton
-    if (iMax < 0.5) throw "Cannot spin less than 0.8 degrees";
-    let difInit = (iMax === Infinity) ? 1 : iMax / Math.round(iMax);
-    let dif = difInit; // So that i is invremented by a factor of iMax
-    let framerate = fps;
+    let dif = 1;
     let speed = (framerate * 60 * dif) / (2 * Math.PI * rpm);
-    let start;
-    console.log(framerate);
-    count = setInterval(() => {
-      hex.draw(Math.cos(i / speed) * size + size); // Make it bigger and smaller
+    let iMax = (Math.PI * speed * until) / 180;
+    fpsArr = [1]; // The first thing pushed is framerate / framerate
+    let start, b4Decrease;
+
+    hex.count = setInterval(() => {
+      hex.draw(Math.cos(i / speed) * hex.size + hex.size); // Make it bigger and smaller
       i = i + dif; // Increment i
-      if (dif === 0) { // If done incrementing
-        clearInterval(count); // Stop looping
-        if (until % 90 === 0 && until % 180 !== 0) ctx.clearRect(0, 0, width, height); // If the hexagon ended as a sliver, get rid of it
-      }
-      if (Math.round(i) >= Math.round(iMax - (10 * Math.PI))) { // If ecceds is 45 degrees from done
-        dif = (Math.acos((i - (iMax - (10 * Math.PI))) / (10 * Math.PI))) * (2  * difInit / Math.PI);
+      if (i >= iMax - (0.25 * Math.PI * speed) && start) { // If ecceds is 45 degrees from done
+        if (!b4Decrease) { b4Decrease = fpsArr.reduce((a, b) => a + b) / fpsArr.length; } //dif; // b4Decrease = dif before slowing down spin
+        dif = hex.framerate(framerate, start, fpsArr) * ((Math.acos(((i - iMax) / (0.25 * Math.PI * speed)) + 1) * 1.9 * b4Decrease) / Math.PI); // Decrease dif **1.9 should be a 2, but it looked wrong**
         if (isNaN(dif)) { // If (i > iMax) <-- This is the same thing
           dif = 0; // Stop spinning
+          clearInterval(hex.count); // Stop looping
+          if (until % 90 === 0 && until % 180 !== 0) hex.clear(); // If the hexagon ended as a sliver, get rid of it
         }
       }
       else if (start) {
-        //console.log("start");
-        //speed = ((1 / ((new Date() - start) / 1000)) * 60 * dif) / (2 * Math.PI * rpm);
-        //start = new Date()
-        //console.log(speed);
-        dif = difInit * (framerate / (1 / ((new Date() - start) / 1000)));//(1 / ((1 / ((new Date() - start) / 1000)) / framerate));
-        //console.log((1 / ((new Date() - start) / 1000)), framerate);
+        dif = hex.framerate(framerate, start, fpsArr);//framerate / (1 / ((new Date() - start) / 1000)); // Change dif to account for lag
       }
-      start = new Date();
+
+      start = new Date(); // start timer to detect fps
     }, 1000 / framerate); // 75 fps
-    return count;
+  };
+
+  hex.framerate = (framerate, start, fpsArr) => {
+    let rate = framerate / (1 / ((new Date() - start) / 1000));
+    fpsArr.push(rate);
+    if (fpsArr.length > 10) fpsArr.shift();
+    return rate;
+  };
+  
+  hex.fps = () => {
+    return 1 / (fpsArr.slice(-1)[0] / framerate);
   };
 
   hex.stop = () => {
-    clearInterval(count);
+    clearInterval(hex.count);
+  };
+
+  hex.clear = () => {
+    ctx.clearRect(0, 0, c.width, c.height);
   };
 };
 
