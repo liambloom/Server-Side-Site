cea26f091e33b1da8e10695fb2491943daab8c17
liambloom <liamrbloom@gmail.com>
2019-08-19 22:20:02 -0400

    Awesome homepage
    Hexagon opening, huge welcome

--- js/homepage.js
+++ js/homepage.js
@@ -8,7 +8,7 @@ c.height = window.innerHeight;
 let draw = () => {
   let grid = new HexGrid({ grid: { allowOverflow: true } });
   //console.log(grid.spin);
-  grid.spin("y", "90deg", "20rpm", "0deg", "(0, 0)", 1, () => {
+  grid.spin("y", "90deg", "20rpm", "0deg", "(0, 0)", "1.5secs", () => {
     document.getElementById("main").style.display = "none";
   });
 };
