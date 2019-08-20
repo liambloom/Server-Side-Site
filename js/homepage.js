548a2a91276256cad236f953291a1cfc7ec5617a
liambloom <liamrbloom@gmail.com>
2019-08-20 02:36:42 -0400

    Homepage looks way cooler

--- js/homepage.js
+++ js/homepage.js
@@ -2,13 +2,18 @@
 import { HexGrid } from "/lib/shapes.js";
 window.HexGrid = HexGrid;
 document.getElementById("Capa_1").style.display = "none";
+document.getElementsByTagName("header")[0].removeChild(document.querySelector("a#logo"));
+/*let bg = (theme.mode === "light") ? themes[name].offWhite : themes[name].offBlack;
+document.getElementById("stop4538").style = `stop-color:${bg};stop-opacity:1`;
+document.getElementById("stop4540").style = `stop-color:${bg};stop-opacity:1`;*/
 let c = document.getElementById("main");
 c.width = window.innerWidth;
 c.height = window.innerHeight;
 let draw = () => {
-  let grid = new HexGrid({ grid: { allowOverflow: true } });
-  grid.spin("y", "90deg", "20rpm", "0deg", "(0, 0)", "1.5secs", () => {
+  let grid = new HexGrid({width: 35, grid: { allowOverflow: true } });
+  grid.spin("y", "90deg", "35rpm", "0deg", "(0, 0)", "1.5secs", () => {
     document.getElementById("main").style.display = "none";
+    document.getElementById("welcome").className = "reveal";
   });
 };
 if (window.themeReady) draw ();
