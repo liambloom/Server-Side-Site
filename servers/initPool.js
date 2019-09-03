97fc706e78fe901de2afd43bf2aa47b4ae880d03
liambloom <liamrbloom@gmail.com>
2019-09-02 21:59:24 -0400

    More async/await (and everything's broken)

--- servers/initPool.js
+++ servers/initPool.js
@@ -8,6 +8,7 @@ const mail = require("./mail");
 const randomKey = require("./randomKey");
 const EventEmitter = require("events");
 const { Pool } = require("pg");
+require("./NodeJSplus")();
 
 global.event = new EventEmitter();
 global.path = req => url.parse(`${req.protocol}://${req.get("host")}${req.originalUrl}`, true);
