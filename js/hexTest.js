8aed3768f54076a9cf93442e1e79ea9a4fb7298f
liambloom <liamrbloom@gmail.com>
2019-10-25 22:28:41 -0400

    Added some more documentation (not much)

--- js/hexTest.js
+++ js/hexTest.js
@@ -344,4 +344,3 @@ document.getElementById("spin").addEventListener("click", event => {
 if (window.themeReady) init();
 else window.addEventListener("themeReady", init);
 hljs.initHighlightingOnLoad();
-console.log(hljs.listLanguages());
