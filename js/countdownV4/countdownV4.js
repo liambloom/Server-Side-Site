cade8aeaec52323efd36064b0c1f4df0a78b25fc
liambloom <liamrbloom@gmail.com>
2019-11-28 15:22:23 -0500

    Update countdownV4.js

--- js/countdownV4/countdownV4.js
+++ js/countdownV4/countdownV4.js
@@ -5,7 +5,7 @@ const set = (bool, number, name) => {
     if (number === 1) document.querySelector(`#${name} .label`).innerHTML = name.replace(/s$/, "");
     else document.querySelector(`#${name} .label`).innerHTML = name;
   }
-  else document.getElementById(name).classList.add("hide");
+  else document.getElementById(name).classList.add("hidden");
   return bool;
 };
 const countdownV4 = first => {
@@ -16,8 +16,9 @@ const countdownV4 = first => {
   const minutes = Math.floor((diff % 3600000) / 60000);
   const seconds = Math.floor((diff % 60000) / 1000);
 
-  if (first) set(set(set(set(days > 0, days, "days") || hours > 0, hours, "hours")|| minutes > 0, minutes, "minutes") || seconds > 0, seconds, "seconds");
-  else if (!set(seconds > 0 || set(minutes > 0 || set(hours > 0 || set(days > 0, days, "days"), hours, "hours"), minutes, "minutes"), seconds, "seconds")) {
+  if (first) set(set(set(set(days, days, "days") || hours, hours, "hours")|| minutes, minutes, "minutes") || seconds, seconds, "seconds");
+  else set(seconds || seconds !== 59 || set(minutes !== 59 || set(hours !== 23 || set(days, days, "days"), hours, "hours"), minutes, "minutes"), seconds, "seconds")
+  if (!(days || hours || minutes || seconds)) {
     document.getElementById("countdown").classList.add("done");
     document.body.classList.add("done");
     clearInterval(interval);
