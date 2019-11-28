49f4d45f27ae9db762f11c6c63bd1caabcf043b6
liambloom <liamrbloom@gmail.com>
2019-11-28 15:02:11 -0500

    Revert "Update countdownV4.js"
    This reverts commit 40c6c500ee70a646a94943e0881e5ff10524f3ca.

--- js/countdownV4/countdownV4.js
+++ js/countdownV4/countdownV4.js
@@ -1,47 +1,26 @@
-const  eventTime = new Date(...JSON.parse(document.getElementById("event_time").value)).getTime();
-const now = new Date().getTime();
-const diff = eventTime - now;
-const until = {};
-until.days = Math.floor(diff / 86400000);
-until.hours = Math.floor((diff % 86400000) / 3600000);
-until.minutes = Math.floor((diff % 3600000) / 60000);
-until.seconds = Math.floor((diff % 60000) / 1000);
-
-const set = (bool, name) => {
-  //let bool = until[name]--; Won't work because of initial setting
-  let number = until[name];
-  console.log(number);
-  if (number < 0) {
-    switch (name) {
-      case "seconds":
-      case "minutes":
-        number = until[name] = 59;
-        break;
-      case "hours":
-        number = until[name] = 23;
-        break;
-      default:
-        console.log("uh oh!", bool, number, name);
-    }
-  }
-  console.log(number);
+let eventTime = new Date(...JSON.parse(document.getElementById("event_time").value)).getTime();
+const set = (bool, number, name) => {
   if (bool) {
     document.querySelector(`#${name} .number`).innerHTML = number.toString();
     if (number === 1) document.querySelector(`#${name} .label`).innerHTML = name.replace(/s$/, "");
     else document.querySelector(`#${name} .label`).innerHTML = name;
   }
-  if (!bool) document.getElementById(name).classList.add("hidden");
-  else document.getElementById(name).classList.remove("hidden");
-  return bool - 1;
+  else document.getElementById(name).classList.add("hide");
+  return bool;
 };
 const countdownV4 = first => {
-  if (first) set(set(set(set(until.days > 0, "days") || until.hours > 0, "hours") || until.minutes > 0, "minutes") || until.seconds > 0, "seconds");
-  else if (!(set(until.seconds-- || set(until.minutes-- || set(until.hours-- || set(until.days--, "days"), "hours"), "minutes"), "seconds"))) {
-    //setTimeout(() => {
-      document.getElementById("countdown").classList.add("done");
-      document.body.classList.add("done");
-      clearInterval(interval);
-    //}, 1000);
+  const now = new Date().getTime();
+  const diff = eventTime - now;
+  const days = Math.floor(diff / 86400000);
+  const hours = Math.floor((diff % 86400000) / 3600000);
+  const minutes = Math.floor((diff % 3600000) / 60000);
+  const seconds = Math.floor((diff % 60000) / 1000);
+
+  if (first) set(set(set(set(days > 0, days, "days") || hours > 0, hours, "hours")|| minutes > 0, minutes, "minutes") || seconds > 0, seconds, "seconds");
+  else if (!set(seconds > 0 || set(minutes > 0 || set(hours > 0 || set(days > 0, days, "days"), hours, "hours"), minutes, "minutes"), seconds, "seconds")) {
+    document.getElementById("countdown").classList.add("done");
+    document.body.classList.add("done");
+    clearInterval(interval);
   }
 };
 countdownV4(true);
