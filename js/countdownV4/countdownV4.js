3434c8c747b46729c262bf698d90bb635e1a7013
liambloom <liamrbloom@gmail.com>
2019-11-28 16:16:34 -0500

    Fixed broken countdown
    The countdown did not work. Itnow works, but I had to rewrite most of the code and it is twice as long, dispite the change being very minor

--- js/countdownV4/countdownV4.js
+++ js/countdownV4/countdownV4.js
@@ -1,28 +1,44 @@
-let eventTime = new Date(...JSON.parse(document.getElementById("event_time").value)).getTime();
-const set = (bool, number, name) => {
-  if (bool) {
-    document.querySelector(`#${name} .number`).innerHTML = number.toString();
-    if (number === 1) document.querySelector(`#${name} .label`).innerHTML = name.replace(/s$/, "");
-    else document.querySelector(`#${name} .label`).innerHTML = name;
+const eventTime = new Date(...JSON.parse(document.getElementById("event_time").value)).getTime();
+const until = {
+  get diff () {
+    return eventTime - new Date().getTime();
+  },
+  get days () {
+    return Math.floor(until.diff / 86400000);
+  },
+  get hours () {
+    return Math.floor((until.diff % 86400000) / 3600000);
+  },
+  get minutes () {
+    return Math.floor((until.diff % 3600000) / 60000);
+  },
+  get seconds () {
+    return Math.floor((until.diff % 60000) / 1000);
   }
-  else document.getElementById(name).classList.add("hide");
-  return bool;
 };
-const countdownV4 = first => {
-  const now = new Date().getTime();
-  const diff = eventTime - now;
-  const days = Math.floor(diff / 86400000);
-  const hours = Math.floor((diff % 86400000) / 3600000);
-  const minutes = Math.floor((diff % 3600000) / 60000);
-  const seconds = Math.floor((diff % 60000) / 1000);
-
-  if (first) set(set(set(set(days > 0, days, "days") || hours > 0, hours, "hours")|| minutes > 0, minutes, "minutes") || seconds > 0, seconds, "seconds");
-  else if (!set(seconds > 0 || set(minutes > 0 || set(hours > 0 || set(days > 0, days, "days"), hours, "hours"), minutes, "minutes"), seconds, "seconds")) {
+const forEachUnit = callback => {
+  for (let i of ["days", "hours", "minutes", "seconds"]) {
+    if (callback(i)) break;
+  }
+};
+const set = name => {
+  const number = until[name];
+  document.querySelector(`#${name} .number`).innerHTML = number.toString();
+  if (number === 1) document.querySelector(`#${name} .label`).innerHTML = name.replace(/s$/, "");
+  else document.querySelector(`#${name} .label`).innerHTML = name;
+  forEachUnit(i => {
+    if (!until[i]) document.getElementById(i).classList.add("hidden");
+    else return true;
+  });
+};
+const countdownV4 = () => {
+  forEachUnit(set);
+  if (!(until.days || until.hours || until.minutes || until.seconds)) {
     document.getElementById("countdown").classList.add("done");
     document.body.classList.add("done");
     clearInterval(interval);
   }
 };
-countdownV4(true);
+countdownV4();
 var interval = setInterval(countdownV4, 1000);
 if (typeof newYear === "object") newYear.init(eventTime);
