c0205e5716bf7148f3e5dc0c1088aaeb9c1a6db6
liambloom <liamrbloom@gmail.com>
2019-12-16 22:44:37 -0500

    Nearly done with custom countdown UI

--- js/countdownV4/custom.js
+++ js/countdownV4/custom.js
@@ -8,18 +8,23 @@ input = options => {
   return newInput + ">";
 };
 const now = new Date();
-document.getElementById("date").innerHTML =  new Date(3333, 10, 22).toLocaleDateString()
-  .replace("11", input({ id: "month", placeholder: "mm", type: "number", maxLength: "2", value: now.getMonth() + 1, required: "required" }))
-  .replace("22", input({ id: "day", placeholder: "dd", type: "number", maxLength: "2", value: now.getDate(), required: "required" }))
-  .replace("3333", input({ id: "year", placeholder: "yyyy", type: "number", maxLength: "4", value: now.getFullYear(), required: "required" }));
-document.getElementById("time").innerHTML = now.toLocaleTimeString()
-  .replace(/(\d{2}(?=:))/, input({ id: "hour", placeholder: "hh", type: "number", maxLength: "2", value: "$1", required: "required" }))
-  .replace(/(\d{2}(?=:))/, input({ id: "minute", placeholder: "mm", type: "number", maxLength: "2", value: "$1", required: "required" }))
-  .replace(/:\d{2}/, "")
-  .replace(/AM|PM/, '<select id="ampm"><option>AM</option><option>PM</option></select>');
+document.querySelectorAll(".date, .fullDate").forEach(e => { 
+  e.innerHTML =  new Date(3333, 10, 22).toLocaleDateString()
+    .replace("11", input({ id: "month", placeholder: "mm", type: "number", maxLength: "2", value: now.getMonth() + 1, required: "required" }))
+    .replace("22", input({ id: "day", placeholder: "dd", type: "number", maxLength: "2", value: now.getDate(), required: "required" }))
+    .replace("/3333", e.classList.contains("fullDate") ? ("/" + input({ id: "year", placeholder: "yyyy", type: "number", maxLength: "4", value: now.getFullYear(), required: "required" })) : "");
+});
+
+[...document.getElementsByClassName("time")].forEach(e => {
+  e.innerHTML = now.toLocaleTimeString()
+    .replace(/(\d{1,2}(?=:))/, input({ id: "hour", placeholder: "hh", type: "number", maxLength: "2", value: "$1", required: "required" }))
+    .replace(/(\d{2}(?=:))/, input({ id: "minute", placeholder: "mm", type: "text", pattern: "\\d{0,2}", maxLength: "2", value: "0".padStart(2, "0"), required: "required" }))
+    .replace(/:\d{2}/, "")
+    .replace(/AM|PM/, '<select id="ampm"><option>AM</option><option>PM</option></select>');
+});
 for (let e of document.getElementsByTagName("input")) {
   e.addEventListener("input", () => {
-    if (e.type === "number") e.value.match(/\D/g, "");
+    if (e.type === "number" || /\\d(?=\*|\+|\{\d+,\d*\})/.test(e.pattern)) { console.log(e.value, e.value.replace(/\D/g, "")); e.value = e.value.replace(/\D/g, ""); }
     //if (e.maxLength < e.value.length) e.value = e.value.substr(0, e.maxLength);
   });
 }
