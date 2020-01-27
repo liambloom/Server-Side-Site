1a535a820d6379560f7321aa1f32ec89ce1f281b
liambloom <liamrbloom@gmail.com>
2020-01-26 22:48:35 -0500

    Admin can now choose player
    This means I don't need to reload several times to test a specific player's ability

--- servers/forbidden.js
+++ servers/forbidden.js
@@ -5,10 +5,14 @@ const url = require("url");
 module.exports = {
   serve (req, res) {
     const game = req.params.game;//new URLSearchParams(url.parse(req.originalUrl).search).get("game");
+    const players = JSON.parse(fs.readFileSync(`./json/forbidden/${game}/players.json`));
+    for (let player in players) {
+      players[player].name = player;
+    }
     serve.custom(req, res, "./forbidden/index", {
       game,
       board: JSON.parse(fs.readFileSync(`./json/forbidden/${game}/board.json`)),
-      players: JSON.parse(fs.readFileSync(`./json/forbidden/${game}/players.json`))
+      players: players
     });
   }
 };
