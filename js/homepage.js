a7beaa404ea373e0b90fdf2d147d48f85ea03b0c
liambloom <liamrbloom@gmail.com>
2020-02-22 01:45:54 -0500

    Moved HexGrid into seperate file

--- js/homepage.js
+++ js/homepage.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-import { HexGrid } from "/lib/Shapes.js";
+import HexGrid from "/lib/HexGrid.js";
 window.HexGrid = HexGrid;
 document.getElementById("Capa_1").style.display = "none";
 document.getElementsByTagName("header")[0].removeChild(document.querySelector("a#logo"));
