147f9e006d09d38a90b999a718a4a4b5dd57e6f7
liambloom <liamrbloom@gmail.com>
2020-02-22 01:45:09 -0500

    Right click menu exists!
    Also interfaces are improved and shapes no longer leave lines or have borders (when canvasSafe is true)
    Unfortunately, HexGrid is no longer working

--- js/hexTest.js
+++ js/hexTest.js
@@ -1,19 +1,20 @@
-import { Shape } from "/lib/Shapes.js";
+import Shape from "/lib/Shapes.js";
 window.Shape = Shape;
 
 window.newShape = (sides, config) => {
   const shape = new Shape(sides, config);
   shape.draw();
+  //Shape.addInterface("color", document.getElementById("ht"), )
+};
+
+const init = () => {
+  newShape(6, { x: 100, y: 105, color: "#0044ff", draggable: true, canvasSafe: false });
+  newShape(3, { x: 200, y: 221, color: "#ff2222", draggable: true });
   Shape.addInterface("x", document.getElementById("x"), parseInt, Math.round);
   Shape.addInterface("y", document.getElementById("y"), parseInt, Math.round);
   Shape.addInterface("width", document.getElementById("widthT"), parseInt, Math.round);
   Shape.addInterface("width", document.getElementById("widthS"), parseInt, Math.round);
   Shape.addInterface("color", document.getElementById("hexColor"), hex => "#" + hex, hex => hex.slice(1));
-};
-
-const init = () => {
-  newShape(6, { x: 100, y: 105, color: "#0044ff", draggable: true });
-  newShape(3, { x: 200, y: 221, color: "#ff2222", draggable: true });
 };
 if (window.themeReady) init();
 else window.addEventListener("themeReady", init);
--- js/homepage.js
+++ js/homepage.js
@@ -18,3 +18,43 @@ let draw = () => {
 };
 if (window.themeReady) draw ();
 else window.addEventListener("themeReady", () => { draw(); });
+
+/*
+Error: Script terminated by timeout at:
+get show@http://127.0.0.1:8080/lib/Shapes.js:252:1
+refresh@http://127.0.0.1:8080/lib/Shapes.js:435:9
+refreshAll@http://127.0.0.1:8080/lib/Shapes.js:535:13
+clear@http://127.0.0.1:8080/lib/Shapes.js:477:11
+_drawApi@http://127.0.0.1:8080/lib/Shapes.js:333:10
+refresh@http://127.0.0.1:8080/lib/Shapes.js:435:43
+refreshAll@http://127.0.0.1:8080/lib/Shapes.js:535:13
+clear@http://127.0.0.1:8080/lib/Shapes.js:477:11
+_drawApi@http://127.0.0.1:8080/lib/Shapes.js:333:10
+refresh@http://127.0.0.1:8080/lib/Shapes.js:435:43
+refreshAll@http://127.0.0.1:8080/lib/Shapes.js:535:13
+clear@http://127.0.0.1:8080/lib/Shapes.js:477:11
+_drawApi@http://127.0.0.1:8080/lib/Shapes.js:333:10
+refresh@http://127.0.0.1:8080/lib/Shapes.js:435:43
+refreshAll@http://127.0.0.1:8080/lib/Shapes.js:535:13
+clear@http://127.0.0.1:8080/lib/Shapes.js:477:11
+_drawApi@http://127.0.0.1:8080/lib/Shapes.js:333:10
+refresh@http://127.0.0.1:8080/lib/Shapes.js:435:43
+refreshAll@http://127.0.0.1:8080/lib/Shapes.js:535:13
+clear@http://127.0.0.1:8080/lib/Shapes.js:477:11
+_drawApi@http://127.0.0.1:8080/lib/Shapes.js:333:10
+refresh@http://127.0.0.1:8080/lib/Shapes.js:435:43
+refreshAll@http://127.0.0.1:8080/lib/Shapes.js:535:13
+clear@http://127.0.0.1:8080/lib/Shapes.js:477:11
+_drawApi@http://127.0.0.1:8080/lib/Shapes.js:333:10
+refresh@http://127.0.0.1:8080/lib/Shapes.js:435:43
+refreshAll@http://127.0.0.1:8080/lib/Shapes.js:535:13
+clear@http://127.0.0.1:8080/lib/Shapes.js:477:11
+draw@http://127.0.0.1:8080/lib/Shapes.js:438:10
+spin/<@http://127.0.0.1:8080/lib/HexGrid.js:154:11
+spin@http://127.0.0.1:8080/lib/HexGrid.js:153:19
+draw@http://127.0.0.1:8080/js/homepage.js:14:8
+@http://127.0.0.1:8080/js/homepage.js:20:52
+window.onload/<@http://127.0.0.1:8080/js/theme.js:61:14
+promise callback*window.onload@http://127.0.0.1:8080/js/theme.js:60:6
+EventHandlerNonNull*@http://127.0.0.1:8080/js/theme.js:39:1
+*/
